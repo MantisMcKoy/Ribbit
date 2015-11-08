b7380e84fc8d6aadaf6ab2ea2bf7a8085e5f3bfc
alex <acfujimoto@gmail.com>
2015-11-08 15:49:52 -0500

    Save to Backend

---
+++ app/src/main/java/com/example/demo/ribbit/ParseConstants.java
@@ -0,0 +1,13 @@
+package com.example.demo.ribbit;
+
+/**
+ * Created by alex on 11/8/15.
+ */
+public final class ParseConstants {
+    //Class names
+
+
+    //Field Names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsrelation";
+}
