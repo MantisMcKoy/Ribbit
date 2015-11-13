5b49f19b91944aada4c6fa7e4ac7aad733ea0559
alex <acfujimoto@gmail.com>
2015-11-13 03:21:25 -0500

    add compatability with Parse

--- app/src/main/java/com/example/demo/ribbit/ParseConstants.java
+++ app/src/main/java/com/example/demo/ribbit/ParseConstants.java
@@ -5,9 +5,18 @@ package com.example.demo.ribbit;
  */
 public final class ParseConstants {
     //Class names
-
+    public static final String CLASS_MESSAGES = "Messages";
 
     //Field Names
     public static final String KEY_USERNAME = "username";
     public static final String KEY_FRIENDS_RELATION = "friendsrelation";
+    public static final String KEY_RECIPIENT_IDS = "recipientIds";
+    public static final String KEY_SENDER_ID = "senderId";
+    public static final String KEY_SENDER_NAME = "senderName";
+    public static final String KEY_FILE = "file";
+    public static final String KEY_FILE_TYPE = "fileType";
+
+    public static final String TYPE_IMAGE = "image";
+    public static final String TYPE_VIDEO = "video";
+
 }
